a7ea0281bcf22c25c5859b3cf901a7fe58433f15
Vysakh <vysakhs@qburst.com>
2020-02-21 17:00:56 +0530

    Added JWT token authorization

---
+++ api-gateway/src/main/java/com/gamestore/apigateway/security/ResourceServerConfig.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gamestore.apigateway.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ *
+ * @author qbuser
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/user-service/oauth/token", "/user-service/users/register")
+                .permitAll()
+                .antMatchers("/**")
+                .authenticated();
+    }
+
+}
